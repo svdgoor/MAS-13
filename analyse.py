d2b9e626ca2d0b585fba74594f90719dcc206c8d
svdgoor <sjoerdvdgoor02@gmail.com>
2024-11-25 11:47:10 +0100

    Merge scripts

--- analyse.py
+++ analyse.py
@@ -2,6 +2,25 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Read the CSV file, skipping the first 5 rows of metadata
+folder = 'experiments/'
+file = max(
+    [file for file in os.listdir(folder)
+        if file.endswith('-table.csv')],
+    key=lambda x: int(x.split('-')[0])
+)
+file_extension = '.csv'
+file_path = folder + file + file_extension
+data = pd.read_csv(file_path, skiprows=6)
+
+
+# # Save the cleaned data to a new CSV file
+cleaned_file_path = folder + file + '_cleaned' + file_extension
+data.to_csv(cleaned_file_path, index=False)
+
+print(f"Cleaned data saved to {cleaned_file_path}")
+
+
 # Load the CSV file
 folder = 'experiments'
 # check all files ending in -table_cleaned and take the highest number
